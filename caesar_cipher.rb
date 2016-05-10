962473bede1eb7956ddeb35f2886d52d1160d8f4
James Helton <trealtrix@gmail.com>
2016-05-09 23:22:28 -0400

    Add uppercase functionality

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,12 @@
 def caesar_cipher(text, shift)
   text = text.split('').collect do |char|
-    ((char.ord - 97 - shift) % 26 + 97).chr
+    if char.ord.between?(97, 97 + 25)
+      ((char.ord - 97 - shift) % 26 + 97).chr
+    elsif char.ord.between?(65, 65 + 25)
+      ((char.ord - 65 - shift) % 26 + 65).chr
+    else
+      char
+    end
   end.join
   text
 end
